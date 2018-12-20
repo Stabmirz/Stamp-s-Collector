d3c34b4cf3117dc884da3eb10ed0c6d397f6e00e
Stabmirz <amirza@techlaunch.io>
2018-12-20 18:31:52 -0500

    updated

--- connection.php
+++ connection.php
@@ -1,7 +1,7 @@
 <?php
    $servername ="localhost";
    $username="root";
-   $password="";
+   $password="Abi@9001";
    $dbname="collection";
    session_start();
    $conn = mysqli_connect($servername,$username,$password,$dbname);
